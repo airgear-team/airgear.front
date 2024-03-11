da5de12d4a6579e3deca172cb5656271687dcf9f
Ilya Deus <deus.ilya@gmail.com>
2024-03-11 22:33:41 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -7,11 +7,35 @@ function App() {
 
     useEffect(() => {
         const headers = new Headers();
-        headers.append('Authorization', 'Bearer');
+        headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJvbmx5dXNlcjEiLCJyb2xlcyI6IlJPTEVfVVNFUiIsImlhdCI6MTcxMDE4NDE3NSwiZXhwIjoxNzEwMjAyMTc1fQ.Mt6RULl6MF54Elxhb2UCMygLgG_lOsiHbeZ_GTx0X6I');
+        headers.append('Content-Type', 'application/json');
 
-        fetch('http://localhost:8080/goods/random-goods', {
-            method: 'GET',
-            headers: headers
+        const body = JSON.stringify({
+            "name": "Sample Item",
+            "description": "This is a sample item description",
+            "price": 100.50,
+            "weekendsPrice": 120.00,
+            "location" : {
+                "regionId" : 4,
+                "settlement" : "ваваів"
+            },
+            "deposit": {
+                "amount": 50.00,
+                "currency": "USD"
+            },
+            "user": {
+                "id": 1
+            },
+            "category" : {
+                "id": 1
+            },
+            "phoneNumber": "+380987654321"
+        });
+
+        fetch('http://localhost:8080/goods/', {
+            method: 'POST',
+            headers: headers,
+            body: body
         })
             .then(response => {
                 if (!response.ok) {
@@ -34,9 +58,9 @@ function App() {
                 <h1>AirGear</h1>
             </header>
             <div className="goods-list">
-                {goods.map(item => (
-                    <GoodsItem key={item.id} item={item} />
-                ))}
+                {/*{goods.map(item => (*/}
+                {/*    <GoodsItem key={item.id} item={item} />*/}
+                {/*))}*/}
             </div>
         </div>
     );
