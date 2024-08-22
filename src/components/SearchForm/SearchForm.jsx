9d39f189d741e010eb5d470425a8f35cd9b128e8
Olena Sinkevych <fuegos1981@gmail.com>
2024-08-22 16:52:30 +0300

    added findLocation for words and handleClick

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,16 +2,61 @@ import React from 'react';
 import MagnifyingIcon from "../../assets/images/icons/Magnifying-glass.svg?react";
 import LocationIcon from "../../assets/images/icons/Location-point.svg?react";
 import styles from './SearchForm.module.scss';
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const SearchForm = () => {
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const navigate = useNavigate();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        fetch(`http://localhost:8089/search/?search=name:${name}, location.settlement:${location}`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                navigate('/search-results', { state: { productCard: data } });
+            })
+            .catch(error => {
+                console.error('Error fetching posts:', error);
+            });
+    };
+
+    const findLocation = (e) => {
+            e.preventDefault();
+            if(e.target.value.length>2){
+                fetch(`http://localhost:8083/locations/?prefix=${e.target.value}`, {
+                    method: 'GET',
+                    credentials: 'include',
+                    headers: {
+                        'Accept': 'application/json'
+                    },
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                    })
+                    .catch(error => {
+                        console.error('Error fetching posts:', error);
+                    });
+            }
+        };
+
     return (
         <div className={styles.container}>
             <div className={styles.search}>
                 <MagnifyingIcon className={styles.icon} />
-                <input type="text" placeholder="Знайти..." className={styles.mainInput} />
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="What are you looking for?" className={styles.mainInput} />
                 <LocationIcon className={styles.icon} />
-                <input type="text" placeholder="Вся Україна" className={styles.locationInput} />
-                <button className={styles.searchButton}>Пошук</button>
+                <input type="text" value={location} onInput={findLocation} onChange={(e) => setLocation(e.target.value)} placeholder="Ukraine" className={styles.locationInput} />
+                <button className={styles.searchButton} onClick={handleClick}>SEARCH</button>
             </div>
         </div>
     );
